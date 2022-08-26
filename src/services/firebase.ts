2e1cfee1e0a56c295dcf2e42db767514966ab830
Diêgo Ferreira <diegofrr.19@gmail.com>
2022-08-26 17:48:18 -0300

    fix: Nova conexão com Firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,9 +2,9 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIrGq_EC5Q3twIK8KxnF31NQ-IyCDyEXI",
-  authDomain: "groupyt-accb7.firebaseapp.com",
-  projectId: "groupyt-accb7",
+  apiKey: "AIzaSyCcMN4HvVavLh1-Vri2A75NJHsP_BvrlVw",
+  authDomain: "groupyt-14d60.firebaseapp.com",
+  projectId: "groupyt-14d60",
 }
 
 if(!firebase.apps.length) {
